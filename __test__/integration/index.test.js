c424b5d24eb3d8826a0c9e434f49c070ae8d19a6
Leonard Li <leonardli.webdeveloper@gmail.com>
2019-03-30 20:39:59 +0000

    First smoke test

--- __test__/integration/index.test.js
+++ __test__/integration/index.test.js
@@ -1,5 +1,13 @@
-describe('Smoke test', () => {
-    test('Smoke', () => {
-        expect(1+1).toBe(2);
-    });
+describe('Index test', () => {
+  const index = require('../../index');
+
+  // Should have server methods.
+  test('Should have server methods', () => {
+    expect(1+1).toBe(2);
+  });
+
+  // Should have client methods.
+  test('Should have client methods', () => {
+    expect(1+2).toBe(3);
+  });
 });
